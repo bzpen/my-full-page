afef622f7a2fe287328b4dffebcee34ac673eac0
kaito kuroba <3118659412@qq.com>
2022-07-20 19:49:32 +0800

    Add: goto github.com

--- example/full-page/src/views/home.vue
+++ example/full-page/src/views/home.vue
@@ -41,11 +41,19 @@ function goto(index: number) {
   fullPage.srcollToPage(page, index)
   fullPage.setPageIndex(index)
 }
+function gotoGitHub(){
+  window.location.href = "https://github.com/bzpen/my-full-page"
+}
 </script>
 <template>
   <div class="view">
     <div ref="allPage" class="view-page">
       <div class="item-1 ">
+        <div class="triangle" @click="gotoGitHub()">
+          <img src="../assets/github.png" alt="">
+          <div :style="{borderColor: 'transparent transparent ' + color[0] + ' '+ color[0]  }"></div>
+
+        </div>
         <h1 class=" text-7xl text-center font-black pt-80">
           My Full Page
         </h1>
@@ -56,6 +64,11 @@ function goto(index: number) {
         </div>
       </div>
       <div class="item-2">
+        <div class="triangle" @click="gotoGitHub()">
+          <img src="../assets/github.png" alt="">
+          <div :style="{borderColor: 'transparent transparent ' + color[1] + ' '+ color[1]  }"></div>
+
+        </div>
         <h1 class=" text-7xl text-center font-black pt-80">
           My Full Page
         </h1>
@@ -66,6 +79,11 @@ function goto(index: number) {
         </div>
       </div>
       <div class="item-3">
+        <div class="triangle" @click="gotoGitHub()">
+          <img src="../assets/github.png" alt="">
+          <div :style="{borderColor: 'transparent transparent ' + color[2] + ' '+ color[2]  }"></div>
+
+        </div>
         <h1 class=" text-7xl text-center font-black pt-80">
           My Full Page
         </h1>
@@ -76,6 +94,11 @@ function goto(index: number) {
         </div>
       </div>
       <div class="item-4">
+        <div class="triangle" @click="gotoGitHub()">
+          <img src="../assets/github.png" alt="">
+          <div :style="{borderColor: 'transparent transparent ' + color[3] + ' '+ color[3]  }"></div>
+
+        </div>
         <h1 class=" text-7xl text-center font-black pt-80">
           My Full Page
         </h1>
@@ -90,6 +113,14 @@ function goto(index: number) {
 </template>
 
 <style scoped>
+
+.view{
+  --bg-color-1 : #cfd5a6;
+  --bg-color-2 : #d5cd79;
+  --bg-color-3 : #d5bc57;
+  --bg-color-4 : #d5a23c;
+
+}
 .view{
   position: relative;
   height: 100vh;
@@ -105,24 +136,59 @@ function goto(index: number) {
 .item-1{
   height: 100%;
   width: 100%;
-  background-color: #cfd5a6;
+  position: relative;
+  background-color: var(--bg-color-1);
 }
 
 .item-2{
   height: 100%;
   width: 100%;
-  background-color: #d5cd79;
+  position: relative;
+  background-color: var(--bg-color-2);
 }
 
 .item-3{
   height: 100%;
   width: 100%;
-  background-color: #d5bc57;
+  position: relative;
+  background-color: var(--bg-color-3);
 }
 
 .item-4{
   height: 100%;
   width: 100%;
-  background-color: #d5a23c;
+  position: relative;
+  background-color: var(--bg-color-4);
 }
+.triangle{
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  border-style: solid;
+  border-width: 70px 100px 70px 100px;
+  border-color: white white transparent transparent;
+  width: 0px;
+  height: 0px;
+  box-shadow: -7px 4px 20px 0px black;;
+}
+.triangle img{
+  position: relative;
+  top:-50px;
+  left: -50px;
+  max-width: 100px;
+  width: 100px;
+  height: 100px;
+}
+.triangle div{
+  position: relative;
+  top: -170px;
+  left: -100px;
+  border-width: 70px 100px 70px 100px;
+  /* border-color: transparent transparent #cfd5a6 #cfd5a6; */
+  cursor: pointer;
+  max-width: 100px;
+  width: 0px;
+  height: 0px;
+}
+
 </style>
